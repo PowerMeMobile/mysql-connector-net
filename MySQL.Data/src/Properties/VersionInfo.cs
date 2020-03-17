c4e23cd7bcf680e80fcf1dfdeaf630095d59adbd
Slava Tretyak <s.tretyak@dev.powermemobile.com>
2020-03-17 13:17:31 +0300

    changed version to 8.4.19

--- MySQL.Data/src/Properties/VersionInfo.cs
+++ MySQL.Data/src/Properties/VersionInfo.cs
@@ -40,6 +40,6 @@ using System.Resources;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("8.3.19")]
-[assembly: AssemblyInformationalVersion("8.3.19")]
+[assembly: AssemblyVersion("8.4.19")]
+[assembly: AssemblyInformationalVersion("8.4.19")]
 [assembly: NeutralResourcesLanguage("en-US")]
